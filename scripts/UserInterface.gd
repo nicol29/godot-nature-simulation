eb34ed1b5a66555d65c0cbec6cd20895e19a8fef
nicol29 <nicolascondrea@gmail.com>
2024-08-25 12:31:53 +0100

    Add reset button to UI

--- scripts/UserInterface.gd
+++ scripts/UserInterface.gd
@@ -1,5 +1,15 @@
 extends Control
 
+@onready var cohesion_slider = $"./ColorRect/MarginContainer/VBoxContainer/Cohesion/CohesionSlider"
+@onready var alignment_slider = $"./ColorRect/MarginContainer/VBoxContainer/Alignment/AlignmentSlider"
+@onready var separation_slider = $"./ColorRect/MarginContainer/VBoxContainer/Separation/SeparationSlider"
+@onready var max_speed_slider = $"./ColorRect/MarginContainer/VBoxContainer/MaxSpeed/MaxSpeedSlider"
+
+var default_cohesion_val := 1.0
+var default_alignment_val := 1.0
+var default_separation_val := 1.0
+var default_max_speed_val := 1.0
+
 # Using signals in combination with a singleton pattern
 func _on_cohesion_slider_value_changed(value):
 	GameSettings.cohesion_weight = value
@@ -15,3 +25,21 @@ func _on_seperation_slider_value_changed(value):
 
 func _on_max_speed_slider_value_changed(value):
 	GameSettings.max_speed = value
+
+
+func _on_button_pressed():
+	reset_input_values()
+
+
+func reset_input_values():
+	# Resetting global script's default value settings
+	GameSettings.cohesion_weight = default_cohesion_val
+	GameSettings.alignment_weight = default_cohesion_val
+	GameSettings.separation_weight = default_separation_val
+	GameSettings.max_speed = default_max_speed_val
+	
+	# Resetting sliders value
+	cohesion_slider.value = default_cohesion_val
+	alignment_slider.value = default_alignment_val
+	separation_slider.value = default_separation_val
+	max_speed_slider.value = default_max_speed_val
